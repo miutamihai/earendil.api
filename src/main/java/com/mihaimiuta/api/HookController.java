af16b10c6ee01c03809cbd31487dfd71d64b643a
miutamihai <miuta.mihai@gmail.com>
2022-06-27 18:22:27 +0300

    [master] Add earendil running

--- src/main/java/com/mihaimiuta/api/HookController.java
+++ src/main/java/com/mihaimiuta/api/HookController.java
@@ -8,13 +8,22 @@ import org.springframework.web.bind.annotation.*;
 public class HookController {
     private void cloneRepo(WebHook payload) throws Exception {
         String branch = payload.getPullRequest().getHead().getRef();
-
         String githubUrl = payload.getRepository().getCloneUrl();
         String command = String.format("git clone -b %s %s", branch, githubUrl);
 
         Process process = Runtime.getRuntime().exec(command);
-        if(process.waitFor() != 0) {
+        if (process.waitFor() != 0) {
             throw new Exception("Cloning repo failed");
+        }
+    }
+
+    private void runEarendil(WebHook payload) throws Exception {
+        String directory = payload.getRepository().getName();
+
+        String command = String.format("earendil run %s/earendil/steps.json", directory);
+        Process process = Runtime.getRuntime().exec(command);
+        if (process.waitFor() != 0) {
+            throw new Exception("Running earendil failed");
         }
     }
 
@@ -22,8 +31,9 @@ public class HookController {
     public ResponseEntity<String> trigger(@RequestBody WebHook payload) {
         try {
             cloneRepo(payload);
+            runEarendil(payload);
         } catch (Exception exception) {
-           return ResponseEntity.internalServerError().body(exception.getMessage());
+            return ResponseEntity.internalServerError().body(exception.getMessage());
         }
         return ResponseEntity.ok(payload.getPullRequest().getUser().getLogin());
     }
